707aad605536c1eca8e479125ef44660b344956d
Pahirathan Nithilan <nithil7985@gmail.com>
2023-11-13 18:54:55 +0530

    commit changes

---
+++ schedule.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="img/logo.png">
+    <link rel="stylesheet" href="style/styles.css">
+    <title>Movies Schedule</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <script src="_.js "></script>
+</head>
+
+<header></header>
+
+<body>
+    <div class="schedule-section">
+        <h1>Schedule</h1>
+        <div class="schedule-dates">
+            <div class="schedule-item">November 08,2023</div>
+            <div class="schedule-item schedule-item-selected">November 09,2023</div>
+            <div class="schedule-item">November 10,2023</div>
+            <div class="schedule-item">November 11,2023</div>
+            <div class="schedule-item">November 12,2023</div>
+        </div>
+        <div class="schedule-table">
+            <table>
+                <tr>
+                    <th>SHOW</th>
+                    <th>SCHEDULE IN CINEMA</th>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                        <h2>LEO</h2>
+                        <i class="far fa-clock"></i> 164m <i class="fas fa-desktop"></i> Regal Cinema, Katubedda
+                        <h3>Moratuwa</h3>
+                        <p>Leo is a 2023 Indian Tamil-language action thriller film directed by Lokesh Kanagaraj and produced by Seven Screen Studio.
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">1:00 PM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">1:00 PM</div>
+                                <div class="schedule-item">6:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">1:00 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                        <h2>LEO</h2>
+                        <i class="far fa-clock"></i> 164m <i class="fas fa-desktop"></i> City Lite Cinema
+                        <h3>Mount Lavinia</h3>
+                        <p>Leo is a 2023 Indian Tamil-language action thriller film directed by Lokesh Kanagaraj and produced by Seven Screen Studio.
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                <td>
+                        <h2>LEO</h2>
+                        <i class="far fa-clock"></i> 164m <i class="fas fa-desktop"></i> Savoy 3D Dolby ATMOS
+                        <h3> Wellawatta</h3>
+                        <p>Leo is a 2023 Indian Tamil-language action thriller film directed by Lokesh Kanagaraj and produced by Seven Screen Studio.
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                <td>
+                        <h2>LEO</h2>
+                        <i class="far fa-clock"></i> 164m <i class="fas fa-desktop"></i> Savoy Premiere (Roxy Cinema)
+                        <h3>Wellawatte</h3>
+                        <p>Leo is a 2023 Indian Tamil-language action thriller film directed by Lokesh Kanagaraj and produced by Seven Screen Studio.
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                <td>
+                        <h2>LEO</h2>
+                        <i class="far fa-clock"></i> 164m <i class="fas fa-desktop"></i> PVR Cinema
+                        <h3>LUXE One Galle Face Mall</h3>
+                        <p>Leo is a 2023 Indian Tamil-language action thriller film directed by Lokesh Kanagaraj and produced by Seven Screen Studio.
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                <td>
+                        <h2>LEO</h2>
+                        <i class="far fa-clock"></i> 164m <i class="fas fa-desktop"></i> VoxX Lite 3D Cinema
+                        <h3>Avissawella</h3>
+                        <p>Leo is a 2023 Indian Tamil-language action thriller film directed by Lokesh Kanagaraj and produced by Seven Screen Studio.
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+
+    </div>
+    <footer></footer>
+
+    <script src="scripts/jquery-3.3.1.min.js "></script>
+    <script src="scripts/owl.carousel.min.js "></script>
+    <script src="scripts/script.js "></script>
+</body>
+
+</html>
